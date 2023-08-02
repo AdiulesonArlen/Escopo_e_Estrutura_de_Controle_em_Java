6d347bcfc4a43247db921abd11e8134c5c3c032f
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2023-08-01 22:28:20 -0300

    Condição Ternaria

---
+++ Controle_Fluxo/src/ResultadoEscolarTernario.java
@@ -0,0 +1,11 @@
+import java.lang.invoke.StringConcatException;
+
+public class ResultadoEscolarTernario {
+    public static void main(String[] args) {
+        int nota = 3;
+        String resultado =  nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
+
+    }
+}
