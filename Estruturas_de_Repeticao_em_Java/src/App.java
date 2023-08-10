a1607d2520b633bbc558bf6c5fd89c6c9a183839
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2023-08-09 22:08:32 -0300

    correção

--- Estruturas_de_Repeticao_em_Java/src/App.java
+++ Estruturas_de_Repeticao_em_Java/src/App.java
@@ -1,9 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< Updated upstream
+
         System.out.println("Hello, World!");
-=======
-        System.out.println("\nHello, World!");
->>>>>>> Stashed changes
+
     }
 }
